cdf5530199af21c33813a3e1d08059ab37c6a9d2
kimsohee-around <koreait.sec2020@gmail.com>
2023-08-21 15:47:53 +0900

    #10 글쓰기 단위테스트

--- src/test/java/jspBoard/SqlSessionTest.java
+++ src/test/java/jspBoard/SqlSessionTest.java
@@ -1,20 +1,33 @@
-package jspBoard;
+package mybatis;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.apache.ibatis.session.SqlSession;
+import org.iclass.dao.CommunityDao;
+import org.iclass.dto.Community;
 import org.junit.jupiter.api.Test;
 
-import mybatis.SqlSessionBean;
-
 class SqlSessionTest {
-	
+
 	@Test
 	void connect() {
 		SqlSession sqlsession = SqlSessionBean.getSession();
 		assertNotNull(sqlsession);
 		
 	}
-
-
+	
+	@Test
+	void write() {
+		CommunityDao dao = CommunityDao.getInstance();
+		long idx = dao.insert(Community.builder()
+				.title("test")
+				.writer("sana")
+				.content("테스트")
+				.build());
+		
+		System.out.println("idx = " + idx);
+		assertNotEquals(0, idx);
+	}
 }
